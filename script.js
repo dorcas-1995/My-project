7c7e32557addcce927134ef3ed5e37b96d36b50a
+dorcus <+wayumulu58@gmail.com>
2022-06-26 22:36:40 +0300

    added a function to display movies

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ function findApiMovies() {
     searchListElement.classList.add("hide-search-list");
   }
 }
+
+
+function MovieListDisplayFunc(movies) {
+  searchListElement.innerHTML = "";
+  let movieposter = "";
+  for (let movie = 0; movie < movies.length; movie++) {
+    let movieListItemElement = document.createElement("div");
+    movieListItemElement.dataset.id = movies[movie].imdbID; // setting movie id in  data-id
+    movieListItemElement.classList.add("search-list-item");
+    if (movies[movie].Poster != "N/A") movieposter = movies[movie].Poster;
+    else movieposter = "image_not_found.png";
+
+    movieListItemElement.innerHTML = `
+        <div class = "search-item-thumbnail">
+            <img src = "${movieposter}">
+        </div>
+        <div class = "search-item-info">
+            <h3>${movies[movie].Title}</h3>
+            <p>${movies[movie].Year}</p>
+        </div>
+        `;
+    searchListElement.appendChild(movieListItemElement);
+  }
+  LoadedMovieDetails();
+}
